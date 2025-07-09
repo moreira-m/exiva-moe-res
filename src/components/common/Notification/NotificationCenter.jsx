5773e882932e616bd7491b0c1e7c342899bd5a05
Matheus Moreira <69878348+moreira-m@users.noreply.github.com>
2025-07-09 08:03:44 -0300

    fix: btn position on notificationCenter and remove unnused  userProfilePopup

--- src/components/common/Notification/NotificationCenter.jsx
+++ src/components/common/Notification/NotificationCenter.jsx
@@ -9,27 +9,27 @@ const NotificationCenter = () => {
       {notifications.map((n) => (
         <div
           key={n.id}
-          className="bg-white text-black p-3 rounded shadow-lg w-72 flex flex-col animate-slide-in relative"
+          className="bg-white text-black p-4 rounded-lg shadow-lg w-72 flex flex-col animate-slide-in relative"
         >
           <button
             onClick={() => removeNotification(n.id)}
-            className="absolute top-1 right-1 text-gray-500 hover:text-gray-700"
+            className="absolute top-1 right-2 text-gray-500 hover:text-gray-700"
           >
             ×
           </button>
           <p className="mb-2 text-sm text-black">
             O {n.applicant.name} solicitou uma vaga para o soulcore de {n.soulCoreName}.
           </p>
-          <div className="flex gap-2 self-end">
+          <div className="flex gap-2 self-end w-full justify-between">
             <button
               onClick={() => respond(n.adId, n.applicant.userId, true, n.id)}
-              className="px-2 py-1 bg-accent-green rounded"
+              className="px-2 py-1 bg-accent-green rounded w-[50%]"
             >
               Aceitar
             </button>
             <button
               onClick={() => respond(n.adId, n.applicant.userId, false, n.id)}
-              className="px-2 py-1 bg-red-600 text-white rounded"
+              className="px-2 py-1 bg-red-600 text-white rounded w-[50%]"
             >
               Recusar
             </button>
